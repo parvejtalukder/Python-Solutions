abf331d47d673bdee91d475c5387fbd98eb6db58
parvejtalukder <mail@parvejhusentalukder.com>
2025-07-31 01:20:24 +0600

    Update: New files

---
+++ three.py
@@ -0,0 +1,5 @@
+x = 3
+y = 6
+z = x + y
+p = "Parvej"
+print(type(z))
---
+++ two.py
@@ -0,0 +1,5 @@
+print("Hello, PY")
+if 5 < 2:
+    print("Five is greater than two!") 
+if 5 > 2:
+    print("Five is greater than two!") 
